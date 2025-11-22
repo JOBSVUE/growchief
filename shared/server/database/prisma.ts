2031e049fb7c2ccd5c55f78736c5207c135116f1
JOBSVUE <178760755+JOBSVUE@users.noreply.github.com>
2025-11-22 05:25:57 -0600

    Update prisma.ts
    Signed-off-by: JOBSVUE <178760755+JOBSVUE@users.noreply.github.com>

--- shared/server/database/prisma.ts
+++ shared/server/database/prisma.ts
@@ -5,7 +5,6 @@ import { PrismaClient } from '@prisma/client';
 export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor() {
     super({
-      datasourceUrl: process.env.DATABASE_URL,
       log: [
         {
           emit: 'event',
@@ -14,7 +13,6 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
       ],
     });
   }
-
   async onModuleInit() {
     await this.$connect();
   }
