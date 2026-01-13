37ce03ae63b2e3bd57fe042f755c8cf6bd9d2e8b
JOBSVUE <178760755+JOBSVUE@users.noreply.github.com>
2026-01-13 05:11:43 -0600

    Update prisma.ts
    Signed-off-by: JOBSVUE <178760755+JOBSVUE@users.noreply.github.com>

--- shared/server/database/prisma.ts
+++ shared/server/database/prisma.ts
@@ -1,10 +1,17 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PrismaPostgres } from '@prisma/adapter-postgres';
+import { Pool } from 'pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor() {
+    const pool = new Pool({
+      connectionString: process.env.DATABASE_URL,
+    });
+    
     super({
+      adapter: new PrismaPostgres(pool),
       log: [
         {
           emit: 'event',
