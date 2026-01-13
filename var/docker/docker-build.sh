f96f1944168e45d179a6abcaf2656c7284a6e1ae
JOBSVUE <178760755+JOBSVUE@users.noreply.github.com>
2026-01-13 13:36:23 -0600

    Update docker-build.sh
    Signed-off-by: JOBSVUE <178760755+JOBSVUE@users.noreply.github.com>

--- var/docker/docker-build.sh
+++ var/docker/docker-build.sh
@@ -3,5 +3,5 @@
 set -o xtrace
 
 docker rmi localhost/growchief || true
-docker build --target dist -t localhost/growchief -f Dockerfile.dev .
-docker build --target devcontainer -t localhost/growchief-devcontainer -f Dockerfile.dev .
+docker build --target dist -t localhost/growchief -f Dockerfile .
+docker build --target devcontainer -t localhost/growchief-devcontainer -f Dockerfile .
